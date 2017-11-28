6b0b9f54b4fd2124d8204e333dfac723da592d28
Adrian Chamorro Silva <32536306+AdrianChSilva@users.noreply.github.com>
2017-11-28 14:03:58 +0100

    Correción de errores

--- Tema07/Ejercicio02_T07.java
+++ Tema07/Ejercicio02_T07.java
@@ -6,27 +6,27 @@ Define un array de 10 caracteres con nombre simbolo y asigna valores a los
 elementos según la tabla que se muestra a continuación. Muestra el contenido
 de todos los elementos del array. ¿Qué sucede con los valores de los elementos
 que no han sido inicializados?
- * @author AdrianChSilva
+ * @author Adrian
  */
-//public class Ejercicio02_T07 {
-//  public static void main(String[] args) {
-//    
-//    char[] car  = new char[12];
-//    
-//    car[0] = 'a';
-//    car[1] = 'x';
-//    car[4] = '@';
-//    car[6] = ' ';
-//    car[7] = '+';
-//    car[8] = 'Q';
-//    
-//    
-//    for(int i = 0; i < 12; i++){
-//      System.out.print(car[i]);
-//    }
-//   
-//            
-//  }
-//}
+public class Ejercicio02_T07 {
+  public static void main(String[] args) {
+    
+    char[] car  = new char[12];
+    
+    car[0] = 'a';
+    car[1] = 'x';
+    car[4] = '@';
+    car[6] = ' ';
+    car[7] = '+';
+    car[8] = 'Q';
+    
+    
+    for(int i = 0; i < 12; i++){
+      System.out.print(car[i]);
+    }
+   
+            
+  }
+}
 
 
