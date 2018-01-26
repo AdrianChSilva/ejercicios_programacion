332a49d46cf2104816e63bfeef429cdbc77f2164
Adrian Chamorro Silva <32536306+AdrianChSilva@users.noreply.github.com>
2018-01-26 09:45:01 +0100

    ej16

---
+++ Tema08_Funciones/Ejercicio16_T08Capicua.java
@@ -0,0 +1,18 @@
+
+import funciones.Ejercicios01Al14;
+
+
+
+/**
+ *
+ * @author Adrian
+ */
+public class Ejercicio16_T08Capicua {
+  public static void main(String[] args) {
+     for (int i = 1; i <= 99999; i++) {
+       if(Ejercicios01Al14.esCapicua(i)){
+         System.out.print(i + " ");
+       }
+     }
+  }
+}
