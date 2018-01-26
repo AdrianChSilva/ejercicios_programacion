da6ced68742051caa8f198a149e1e140bfba31c6
Adrian Chamorro Silva <32536306+AdrianChSilva@users.noreply.github.com>
2018-01-26 09:37:54 +0100

    actualizacion

---
+++ Tema08_Funciones/Ejercicio15_T08Primos.java
@@ -0,0 +1,19 @@
+
+/**
+ * Muestra los números primos que hay entre 1 y 1000.
+ *
+ * @author Adrian
+ */
+import funciones.Ejercicios01Al14;
+
+public class Ejercicio15_T08Primos {
+
+  public static void main(String[] args) {
+    for (int i = 1; i <= 1000; i++) {
+       if (Ejercicios01Al14.esPrimo(i)) {
+      System.out.print(i + " ");
+    }
+    }
+
+  }
+}
