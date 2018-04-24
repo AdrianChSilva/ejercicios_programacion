8fed0de212bb7cfda4387ae58b2cbf191de359d8
Adrian Chamorro Silva <32536306+AdrianChSilva@users.noreply.github.com>
2018-04-24 19:27:44 +0200

    pequeños cambios

--- examenesResueltos/ExamenRecDAW22Marz18/Ej02_RecorredArrayPosicion.java
+++ examenesResueltos/ExamenRecDAW22Marz18/Ej02_RecorredArrayPosicion.java
@@ -3,6 +3,13 @@
  */
 
 /**
+ * Implementa una función con nombre nEsimo que busque el número que hay dentro de un array
+ * bidimensional en la posición n-ésima contando de izquierda a derecha y de arriba abajo, como si
+ * se estuviera leyendo. El primer elemento es el 0. Si la posición donde se busca no existe en el
+ * array, la función debe devolver -1. Se debe entregar tanto el código de la función como el código
+ * de prueba que la usa. La cabecera de la función es la siguiente: public static int nEsimo(int[][]
+ * n, int posicion)
+ * 
  * @author Adrian
  *
  */
@@ -11,7 +18,7 @@ public class Ej02_RecorredArrayPosicion {
   public static void main(String[] args) {
     // TODO Auto-generated method stub
     int[][] array = new int[4][5];
-    
+
     for (int fila = 0; fila < 4; fila++) {// en este bucle introducimos los 20 números
       for (int columna = 0; columna < 5; columna++) {
         array[fila][columna] = (int) (Math.random() * 100 + 100);
@@ -27,7 +34,7 @@ public class Ej02_RecorredArrayPosicion {
       }
       System.out.println();
     }
-    System.out.print("comprobamos " + nEsimo(array,10));
+    System.out.print("comprobamos " + nEsimo(array, 10));
   }
 
   public static int nEsimo(int[][] n, int posicion) {
@@ -36,20 +43,20 @@ public class Ej02_RecorredArrayPosicion {
     int columna;
     int resultado = 0;
     int aux = -1;
-    
+
     for (fila = 0; fila < n.length; fila++) {
-      for (columna = 0; columna < n.length+1; columna++) {
+      for (columna = 0; columna < n.length + 1; columna++) {
         aux++;
-          if (aux == posicion) {
-            resultado = n[fila][columna];
-          }
-          if(resultado == 0) {
-            resultado = -1;
-          }
+        if (aux == posicion) {
+          resultado = n[fila][columna];
+        }
+        if (resultado == 0) {
+          resultado = -1;
         }
       }
-      System.out.println();
-    
+    }
+    System.out.println();
+
     return resultado;
   }
 
