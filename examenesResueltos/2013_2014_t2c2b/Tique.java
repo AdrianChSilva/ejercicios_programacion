2fbf795bf020258ef0f8b53f023dafc51445df41
adrian chamorro silva <adrianchamorrosilva@gmail.com>
2018-05-09 12:39:22 +0200

    Ej04 y examen

---
+++ examenesResueltos/2013_2014_t2c2b/Tique.java
@@ -0,0 +1,81 @@
+
+public class Tique {
+  private static int codigo = 0;
+  private int numero;
+  private int horas;
+  private int minutos;
+  private double precio = 0.02;
+  private static float recaudacion;
+
+  
+  
+  public static int getCodigo() {
+    return codigo;
+  }
+
+  public static void setCodigo(int codigo) {
+    Tique.codigo = codigo;
+  }
+  static int obtenerNumero() {
+    Tique.setCodigo(Tique.getCodigo() + 1);
+    return Tique.getCodigo();
+  }
+  public Tique(int horas, int minutos) {
+    this.numero = Tique.obtenerNumero();
+    this.horas = horas;
+    this.minutos = minutos;
+  }
+
+  public int getNumero() {
+    return numero;
+  }
+
+  public void setNumero(int numero) {
+    this.numero = numero;
+  }
+
+  public int getHoras() {
+    return horas;
+  }
+
+  public void setHoras(int horas) {
+    this.horas = horas;
+  }
+
+  public int getMinutos() {
+    return minutos;
+  }
+
+  public void setMinutos(int minutos) {
+    this.minutos = minutos;
+  }
+
+  public double getPrecio() {
+    return precio;
+  }
+
+  public void setPrecio(double precio) {
+    this.precio = precio;
+  }
+
+
+
+  public static float getRecaudacion() {
+    return recaudacion;
+  }
+
+  public static void setRecaudacion(float recaudacion) {
+    Tique.recaudacion = recaudacion;
+  }
+
+
+  public  void paga(int horasP, int minutosP) {
+    String resultado;
+    float x = (float) (((horasP * 60 + minutosP) - (this.getHoras() * 60 + this.getMinutos())) * this.getPrecio());
+    resultado = "tique nº" + this.numero+ ". Debe pagar "  + x + "€. Gracias";
+    Tique.setRecaudacion(Tique.getRecaudacion() +x);
+    System.out.println(resultado);
+    
+  }
+
+}
