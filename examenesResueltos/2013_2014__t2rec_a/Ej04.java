5d5cf37d60aa58a38e1902f443bc924059e538bc
adrian chamorro silva <adrianchamorrosilva@gmail.com>
2018-05-09 13:17:06 +0200

    examen (sin acabar)

---
+++ examenesResueltos/2013_2014__t2rec_a/Ej04.java
@@ -0,0 +1,19 @@
+/**
+ * Modifica el ejercicio Expocoches Campanillas realizado en clase añadiendo las siguientes mejoras:
+ * a) El precio de la entrada no será el mismo para todas las zonas. En el momento en que se define
+ * una zona, se deberá indicar - además del aforo - el precio de la entrada. b) Al realizar la
+ * venta, el cajero/a indicará el precio total según la zona elegida y el número de personas, luego
+ * aceptará el dinero y por último devolverá el cambio. Realiza las comprobaciones que sean
+ * necesarias. Nota: Será necesario modificar tanto la clase Zona como el programa principal.
+ * 
+ * @author adrian.chamorrosilva
+ *
+ */
+public class Ej04 {
+
+  public static void main(String[] args) {
+    // TODO Auto-generated method stub
+
+  }
+
+}
