c2db18f1302d7bf8e8105f4691f65702c819b066
adrian chamorro silva <adrianchamorrosilva@gmail.com>
2018-05-09 13:52:45 +0200

    Ej04 - Terminado

--- examenesResueltos/2013_2014__t2rec_a/Ej04.java
+++ examenesResueltos/2013_2014__t2rec_a/Ej04.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 /**
  * Modifica el ejercicio Expocoches Campanillas realizado en clase añadiendo las siguientes mejoras:
  * a) El precio de la entrada no será el mismo para todas las zonas. En el momento en que se define
@@ -13,7 +15,62 @@ public class Ej04 {
 
   public static void main(String[] args) {
     // TODO Auto-generated method stub
+    Scanner s = new Scanner(System.in);
 
+    Zona principal = new Zona(1000, 20);
+    Zona compraVenta = new Zona(200, 50);
+    Zona vip = new Zona(25, 100);
+
+    int opcion = 0;
+    int opcion2 = 0;
+    int n = 0;
+
+    do {
+      System.out.println("\n\nEXPOCOCHES CAMPANILLAS");
+      System.out.println("1. Mostrar número de entradas libres");
+      System.out.println("2. Vender entradas");
+      System.out.println("3. Salir");
+      System.out.println("Elige una opción: ");
+
+      opcion = Integer.parseInt(s.nextLine());
+
+      if (opcion == 1) {
+        System.out.println("\n\nEn la zona principal hay " + principal.getEntradasPorVender()
+            + ". Cada entrada cuesta " + principal.getPrecio());
+        System.out.println("En la zona de compra venta hay " + compraVenta.getEntradasPorVender()
+            + ". Cada entrada cuesta " + compraVenta.getPrecio());
+        System.out.println("En la zona vip hay " + vip.getEntradasPorVender()
+            + ". Cada entrada cuesta " + vip.getPrecio());
+      }
+
+      if (opcion == 2) {
+        System.out.println("\n\n1. Principal " + ". Cada entrada cuesta " + principal.getPrecio());
+        System.out.println("2. Compra-venta" + ". Cada entrada cuesta " + compraVenta.getPrecio());
+        System.out.println("3. Vip" + ". Cada entrada cuesta " + vip.getPrecio());
+        System.out.print("Elige la zona para la que quieres comprar las entradas: ");
+
+        opcion2 = Integer.parseInt(s.nextLine());
+
+        System.out.print("¿Cuántas entradas quieres? ");
+        n = Integer.parseInt(s.nextLine());
+        int paga = 0;
+        System.out.print("Introduzca el dinero ");
+        paga = Integer.parseInt(s.nextLine());
+        switch (opcion2) {
+          case 1:
+            principal.vender(n, paga);
+            break;
+          case 2:
+            compraVenta.vender(n, paga);
+            break;
+          case 3:
+            vip.vender(n, paga);
+            break;
+          default:
+        }
+      }
+    } while (opcion < 3); // menú principal
   }
-
 }
+
+
