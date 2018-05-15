bc7fbf94308a3b37ac704740fc7976a0f13a3880
Adrian Chamorro Silva <32536306+AdrianChSilva@users.noreply.github.com>
2018-05-15 18:30:40 +0200

    Corregido

--- examenesResueltos/prog_2015_2016_t2rec/Ej4_EstirarArray.java
+++ examenesResueltos/prog_2015_2016_t2rec/Ej4_EstirarArray.java
@@ -11,53 +11,64 @@
 public class Ej4_EstirarArray {
 
   public static void main(String[] args) {
-    // TODO Auto-generated method stub
-    int[][] array = new int[5][5];
 
-    
-    for (int filas = 0; filas < 5; filas++) {
-      for (int columnas = 0; columnas < 5; columnas++) {
-        array[filas][columnas] = (int) (Math.random() * 101) + 100;
-        System.out.printf("%5d   ", array[filas][columnas]);
+    int x = (int) (Math.random() * 4) + 2;
+    int y = (int) (Math.random() * 4) + 2;
+    int cont = 0;
+
+    int[][] matriz = new int[x][y];
+    for (int i = 0; i < matriz.length; i++) {
+      for (int j = 0; j < matriz[0].length; j++) {
+        matriz[i][j] = (int) (Math.random() * 9) + 1;
+        System.out.printf("%3d   ", matriz[i][j]);
+        cont++;
       }
       System.out.println();
     }
-    
-    int[] resultado = estira(array);
-    for (int i = 0; i < 25; i++) {
-      System.out.print(resultado[i]+ " ");
-    }
-    
-    
-  }
-  public static int[] estira(int[][] n) {
-    int[] resultado = new int[25];
-    for(int filas = 0; filas<5;filas++) {
-      
-      for (int columnas = 0; columnas < 5; columnas++) {
-        if(filas == 0) {
-          resultado[columnas] = n[filas][columnas];
-        }
 
-        if(filas == 1) {
-          resultado[columnas+5] = n[filas][columnas];
-        }
-        if(filas == 2) {
-          resultado[columnas+10] = n[filas][columnas];
-        }
-        
-        if(filas == 3) {
-          resultado[columnas+15] = n[filas][columnas];
-        }
-        
-        if(filas == 4) {
-          resultado[columnas+20] = n[filas][columnas];
-        }
+    System.out.println();
+
+
+
+    int[] resultado = estira(matriz);
+    for (int i = 0; i < resultado.length; i++) {
+      System.out.print(resultado[i] + " ");
+    }
+
+
+  }
+
+  public static int[] estira(int[][] n) {
+    int cont = 0;
+    for (int i = 0; i < n.length; i++) {
+      for (int j = 0; j < n[0].length; j++) {
+        cont++;
       }
     }
-    
+    // ESTIRA EL ARRAY EN ORDEN FILA A FILA
+    int[] resultado = new int[cont];
+    int contador = 0;
+    for (int i = 0; i < n.length; i++) {
+      for (int j = 0; j < n[0].length; j++) {
+        for (int f = contador; f < resultado.length; f++) {
+          resultado[f] = n[i][j];
+
+        }
+        contador++;
+      }
+    }
+
     return resultado;
-    
+
   }
-  
+  // ESTIRA EL ARRAY POR ORDEN EN COLUMNAS
+  // for (int i = 0; i < n[0].length; i++) {
+  // for (int j = 0; j < n.length; j++) {
+  // for (int f = contador; f < resultado.length; f++) {
+  // resultado[f] = n[j][i];
+  //
+  // }
+  // contador++;
+  // }
+  // }
 }
