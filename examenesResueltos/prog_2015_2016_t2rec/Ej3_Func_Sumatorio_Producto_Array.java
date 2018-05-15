a51efbb56ff1db72bcb7206efcae32d7abb7f260
Adrian Chamorro Silva <32536306+AdrianChSilva@users.noreply.github.com>
2018-05-15 18:38:14 +0200

    Mejorado

--- examenesResueltos/prog_2015_2016_t2rec/Ej3_Func_Sumatorio_Producto_Array.java
+++ examenesResueltos/prog_2015_2016_t2rec/Ej3_Func_Sumatorio_Producto_Array.java
@@ -3,13 +3,19 @@ public class Ej3_Func_Sumatorio_Producto_Array {
 
   public static void main(String[] args) {
     // TODO Auto-generated method stub
-    int[] array = {8,2,7,1};
-    System.out.print("Sumatorio: " + sumatorio(array));
+    int x = (int) (Math.random() * 8) + 2;
+    int[] array1 = new int[x];
+    for (int i = 0; i < array1.length; i++) {
+      array1[i]=(int) (Math.random() * 8) + 1;
+      System.out.print(array1[i] + " ");
+    }
+    System.out.println();
+    System.out.print("Sumatorio: " + sumatorio(array1));
     
   }
   public static int sumatorio(int[] n) {
     int suma = 0;
-    for(int i = 0; i < 4; i++) {
+    for(int i = 0; i < n.length; i++) {
       suma += n[i];
     }
     return suma;
